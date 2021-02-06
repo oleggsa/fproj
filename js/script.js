920c2cb849402937f3f2e6ab9ecb1f493d548f23
Oleg <oleggsa@gmail.com>
2021-02-06 21:51:12 +0200

    first edit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
+console.log(2 + 4 !== '6');
+
+const isChecked = false,
+      isClose = true;
+
+console.log(!isChecked && isClose);
